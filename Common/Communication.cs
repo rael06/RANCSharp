58e738b1d6f4b175f13ef1955954a9e6fde57e8a
rael06 <47027233+rael06@users.noreply.github.com>
2019-09-25 22:19:27 +0200

    Add files via upload

---
+++ Common/Communication.cs
@@ -0,0 +1,26 @@
+﻿using Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+	public class Communication
+	{
+		public string Origin { get; set; }
+		public EType Type { get; set; }
+		public ETarget Target { get; set; }
+		public dynamic Content { get; set; }
+		public bool Success { get; set; }
+
+		public override string ToString()
+		{
+			Content = Content ?? "null";
+			return $"Communication from {Origin} = \n" +
+				$"Type : {Type},\n" +
+				$"Target : {Target}, \n" +
+				$"Content : {(Content as object).ToString()}, \n" +
+				$"Success : {Success.ToString()}";
+		}
+	}
+}
